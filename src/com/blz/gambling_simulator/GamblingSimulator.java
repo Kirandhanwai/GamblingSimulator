50de2255fd441d3f671a8c81edcfbb1b05ef19d7
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-09 13:48:20 +0530

    Modified : Player is Playing First Bet

--- src/com/blz/gambling_simulator/GamblingSimulator.java
+++ src/com/blz/gambling_simulator/GamblingSimulator.java
@@ -2,25 +2,29 @@ package com.blz.gambling_simulator;
 
 public class GamblingSimulator {
 	
-	static int stake = 100;
-	static int Bet = 1;
+	static int STAKE_PER_DAY = 100;
+	static int BET_PER_GAME = 1;
 	public static void main (String[] Args) {
 		System.out.println("Weclome To Gambling Simulator");
+		{
 		int stake = 100;
 		int bet =1;
+	
 		double Betting = Math.floor(Math.random() * 100) % 2;
+		
 		if ( Betting == bet ) {
 			stake ++;
-			System.out.println("Gambler is Winning $1");
-			System.out.println("stake= " + stake);
+			System.out.println("Gambler is Winning");	
 		}
+		
 		else {
 			stake --;
-			System.out.println("Gambler is loseing $1");
+			System.out.println("Gambler is Loseing");
 			
 		}
 		}
 	}
+}
 	
 			
 	
